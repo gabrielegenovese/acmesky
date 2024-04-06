afabf3c18319373733bc46924ca5ebfdaea91908
jjak0b <jacopo.rimediotti@studio.unibo.it>
2024-04-06 13:49:36 +0200

    Add GetAirportsByIds

--- src/ACMESkyService/repository/airports/airports.go
+++ src/ACMESkyService/repository/airports/airports.go
@@ -43,3 +43,53 @@ func GetAirports(query string) ([]entities.Airport, error) {
 	}
 	return airports, nil
 }
+
+func GetAirportsById(ids []string) ([]entities.Airport, error) {
+	db := dbClient.GetInstance()
+	var airports []entities.Airport
+	var rows *sql.Rows
+	var err error
+
+	if db == nil {
+		fmt.Println("ERROR NIL")
+	}
+
+	if len(ids) > 0 {
+		sqlStr := "SELECT * FROM Airports WHERE AirportID IN ("
+		vals := []interface{}{}
+		for _, id := range ids {
+			sqlStr += "?,"
+			vals = append(vals, id)
+		}
+
+		//trim the last ,
+		sqlStr = sqlStr[0 : len(sqlStr)-1]
+		sqlStr += ")"
+
+		//prepare the statement
+		rows, err = db.Query(sqlStr, vals...)
+	} else {
+		rows, err = db.Query("SELECT * FROM Airports ORDER BY Name ASC")
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("airportsByIDs: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var airport entities.Airport
+		if err := rows.Scan(&airport.AirportID, &airport.Name, &airport.City); err != nil {
+			return nil, fmt.Errorf("airportsByIDs: %v", err)
+		}
+
+		airports = append(airports, airport)
+	}
+	if err := rows.Err(); err != nil {
+		if err == sql.ErrNoRows {
+			return airports, nil
+		}
+		return nil, fmt.Errorf("airportsByIDs: %v", err)
+	}
+	return airports, nil
+}
