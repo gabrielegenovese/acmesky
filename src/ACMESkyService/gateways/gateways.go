b69fe9d23cc137238941ca44c9ec280351b56030
jjak0b <jacopo.rimediotti@studio.unibo.it>
2024-05-27 20:25:34 +0200

    Aggiunto cors middleware in ACMESky services

--- src/ACMESkyService/gateways/gateways.go
+++ src/ACMESkyService/gateways/gateways.go
@@ -25,10 +25,27 @@ import (
 // @externalDocs.url          https://swagger.io/resources/open-api/
 func Listen() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	v1 := r.Group("/api/v1")
 	{
 		flight_subscription.Listen(v1)
 	}
 
 	r.Run("localhost:8090")
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
